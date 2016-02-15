9b7901c89852ad837af8f0d76dacc69843a5714f
Thomas Victoria <thomas.victoria@hetic.net>
2016-02-15 17:18:52 +0100

    Convertion api

--- src/AppBundle/Controller/ParisRestaurantController.php
+++ src/AppBundle/Controller/ParisRestaurantController.php
@@ -6,6 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\ParisRestaurant;
 use AppBundle\Form\ParisRestaurantType;
 
@@ -19,15 +21,22 @@ class ParisRestaurantController extends Controller
   /**
      * Finds and displays a ParisRestaurant entity.
      *
-     * @Route("/{id}", name="restaurant_show")
+     * @Route("/{id}/", name="restaurant_show")
      * @Method("GET")
      */
   public function showAction(ParisRestaurant $parisRestaurant)
   {
 
-    return $this->render('parisrestaurant/show.html.twig', array(
-      'parisRestaurant' => $parisRestaurant,
-    ));
+    $response = array(
+      'id'       => $parisRestaurant->getId(),
+      'category' => $parisRestaurant->getCategory(),
+      'adresse'  => $parisRestaurant->getAdresse(),
+      'lat'      => $parisRestaurant->getLatitude(),
+      'long'     => $parisRestaurant->getLongitude(),
+    );
+        
+    return new JsonResponse($response);
+
   }
 
 }
--- src/AppBundle/Controller/ParisSchoolController.php
+++ src/AppBundle/Controller/ParisSchoolController.php
@@ -6,8 +6,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\ParisSchool;
 use AppBundle\Form\ParisSchoolType;
+
 
 /**
  * ParisSchool controller.
@@ -16,54 +19,67 @@ use AppBundle\Form\ParisSchoolType;
  */
 class ParisSchoolController extends Controller
 {
-    /**
+  /**
      * Lists all ParisSchool entities.
      *
      * @Route("/", name="school_index")
      * @Method("GET")
      */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+  public function indexAction()
+  {
+    $em = $this->getDoctrine()->getManager();
+    $query = $em->createQuery(
+      'SELECT s
+        FROM AppBundle:ParisSchool s'
+    );
+    $school = $query->getArrayResult();
 
-        $parisSchools = $em->getRepository('AppBundle:ParisSchool')->findAll();
+    return new JsonResponse($school);
 
-        return $this->render('parisschool/index.html.twig', array(
-            'parisSchools' => $parisSchools,
-        ));
-    }
+  }
 
-    /**
+  /**
      * Finds and displays a ParisSchool entity.
      *
-     * @Route("/{uai}", name="school_show")
+     * @Route("/{uai}/", name="school_show")
      * @Method("GET")
      */
-    public function showAction(ParisSchool $parisSchool)
-    {
+  public function showAction(ParisSchool $parisSchool)
+  {
 
-        $em = $this->getDoctrine()->getRepository('AppBundle:ParisRestaurant');
+    $school = array(
+      'id'       => $parisSchool->getId(),
+      'uai'      => $parisSchool->getUai(),
+      'name'     => $parisSchool->getName(),
+      'adresse'  => $parisSchool->getAdresse(),
+      'cp'       => $parisSchool->getCp(),
+      'lat'      => $parisSchool->getLatitude(),
+      'long'     => $parisSchool->getLongitude(),
+    );
 
-        $lat = $parisSchool->getLatitude();
-        $long = $parisSchool->getLongitude();
+    $em = $this->getDoctrine()->getRepository('AppBundle:ParisRestaurant');
 
-        $query = $em->createQueryBuilder('r')
-          ->where('r.latitude < :latup')
-          ->setParameter('latup', $lat+0.001)
-          ->andWhere('r.latitude > :latdown')
-          ->setParameter('latdown', $lat-0.001)
-          ->andWhere('r.longitude < :longup')
-          ->setParameter('longup', $long+0.001)
-          ->andWhere('r.longitude > :longdown')
-          ->setParameter('longdown', $long-0.001)
-          ->getQuery();
+    $lat = $parisSchool->getLatitude();
+    $long = $parisSchool->getLongitude();
 
-        $restaurants = $query->getResult();
+    $query = $em->createQueryBuilder('r')
+      ->where('r.latitude < :latup')
+      ->setParameter('latup', $lat+0.001)
+      ->andWhere('r.latitude > :latdown')
+      ->setParameter('latdown', $lat-0.001)
+      ->andWhere('r.longitude < :longup')
+      ->setParameter('longup', $long+0.001)
+      ->andWhere('r.longitude > :longdown')
+      ->setParameter('longdown', $long-0.001)
+      ->getQuery();
 
-        return $this->render('parisschool/show.html.twig', array(
-            'parisSchool' => $parisSchool,
-            'restaurants' => $restaurants
-        ));
-    }
+    $restaurants = $query->getArrayResult();
+
+    return new JsonResponse(array(
+      'school'      => $school,
+      'restaurants' => $restaurants
+    ));
+
+  }
 
 }
