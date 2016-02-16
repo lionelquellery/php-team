10001f7bf7487e26c42020d9fd9e9b3652eae7bb
Lionel quellery <lionel.quellery@hetic.net>
2016-02-17 00:24:04 +0100

    unit test from travis

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     *
+     */
     public function testIndex()
     {
         $client = static::createClient();
@@ -14,5 +17,6 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
     }
 }
