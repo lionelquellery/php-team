02da3abb035075cd9deb1a4b5b05db4d4d1945fa
Lionel quellery <lionel.quellery@hetic.net>
2016-02-17 09:32:57 +0100

    unit test functional test

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -11,11 +11,11 @@ class DefaultControllerTest extends WebTestCase
      */
     public function testIndex()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        //test of all route  user (functional test)
+        $yann = static::createClient();
+        $yann->request('GET','/school/');
+        $this->assertEquals('200',$yann->getResponse()->getStatusCode());
+        
 
     }
 }
