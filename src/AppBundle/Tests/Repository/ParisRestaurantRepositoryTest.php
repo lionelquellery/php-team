b958eb0fed1f59056302fab2f483fbd6a41bb8fe
Lionel quellery <lionel.quellery@hetic.net>
2016-02-20 17:20:49 +0100

    add paris restaurant test
    test from paris

---
+++ src/AppBundle/Tests/Repository/ParisRestaurantRepositoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Tests\Repository;
+
+use AppBundle\Entity\ParisRestaurant;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ParisRestaurantRepositoryTest extends WebTestCase
+{
+    private $repo;
+
+    protected function setUp()
+    {
+        self::bootKernel();
+
+        $this->repo = static::$kernel->getContainer()
+            ->get('doctrine.orm.entity_manager')
+            ->getRepository('AppBundle:ParisSchool');
+    }
+
+    public function testgetPerimeter()
+    {
+        $long = null;
+        $lat = null;
+        $distance = null;
+
+        $perimeter = $this->repo->getPerimeter($lat, $long, $distance);
+
+    }
+
+
+
+
+
+
+
+}
