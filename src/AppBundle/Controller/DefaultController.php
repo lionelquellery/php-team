2c9db58f58009d11280a732bd98d503d320f7fe4
Thomas Victoria <thomas.victoria@hetic.net>
2016-02-25 19:13:35 +0100

    changements

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,25 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
+  /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ));
-    }
+  public function indexAction(Request $request)
+  {
+
+
+    $message = \Swift_Message::newInstance()
+      ->setSubject('Hello Email')
+      ->setFrom('send@example.com')
+      ->setTo('vtom.pro@gmail.com')
+      ->setBody('yolo');
+    
+    $this->get('mailer')->send($message);
+
+
+    // replace this example code with whatever you need
+    return $this->render('default/index.html.twig', array(
+      'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+    ));
+  }
 }
